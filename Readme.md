8ca4b437cd0ec0034f8c017c559cd5bca95880e0
sylhare <sylhareng@gmail.com>
2018-03-02 11:14:53 -0500

    Adding add and delete

--- Readme.md
+++ Readme.md
@@ -1,5 +1,6 @@
 # Meteor todo
 [![npm version](https://badge.fury.io/js/meteor-todo.svg)](https://badge.fury.io/js/meteor-todo)
+[![Generic badge](https://img.shields.io/badge/github-sylhare-blue.svg)](https://github.com/Sylhare/meteor-todo)
 
 This is a basic todo app with the [meteor platform](https://www.meteor.com/) and [React](https://reactjs.org/).
 
@@ -22,14 +23,18 @@ meteor
 ```
 
 The the app should be running at [localhost:3000/](http://localhost:3000/).
-Once the app is started, you can open a new terminal and manually add a todo item with:
+
+## Getting started
+
+Once the app is started, you can open a new terminal and manually add a todo item with this command:
 
 ```bash
 meteor mongo
 ```
 
-Then add a new task with:
+A mongo DB is created automatically with meteor, add a new task that will be store in this database with:
 
 ```bash
 db.tasks.insert({ text: "Hello world!", createdAt: new Date() });
 ```
+
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -12,11 +13,33 @@ class App extends Component {
         ));
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
     render() {
         return (
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Type to add new tasks"
+                        />
+                    </form>
                 </header>
 
                 <ul>
@@ -29,6 +52,6 @@ class App extends Component {
 
 export default withTracker(() => {
     return {
-        tasks: Tasks.find({}).fetch(),
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     };
 })(App);
