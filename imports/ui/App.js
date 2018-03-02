687dd13160f9635c984f8c1aed0bbc774de04741
sylhare <sylhareng@gmail.com>
2018-03-02 13:38:49 -0500

    Updating UI

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,8 +7,20 @@ import Task from './Task.js';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -28,11 +40,28 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     render() {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List - {this.props.incompleteCount}</h1>
+
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide
+                    </label>
+
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                         <input
                             type="text"
@@ -53,5 +82,6 @@ class App extends Component {
 export default withTracker(() => {
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     };
 })(App);
